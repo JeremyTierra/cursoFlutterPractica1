7756afcc6753f04e720afc9ad6cd8419b12495b8
Jeremy Tierra <jeremycarvajal.2003@gmail.com>
2024-04-27 16:51:13 -0500

    PROYECTO 1

---
+++ lib/home.dart
@@ -0,0 +1,105 @@
+import 'package:curso_basico_flutter/dashboard.dart';
+import 'package:curso_basico_flutter/pagina_con_estado.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  void _onPressedDashboard(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) {
+        return const Dashboard();
+      },
+    ));
+  }
+
+void _onPressedEstado(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) {
+        return const PaginaConEstado();
+      },
+    ));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: GestureDetector(
+          onTap: () {
+            _onPressedDashboard(context);
+          },
+          child: const Center(
+              child: Text(
+            "Curso de Flutter",
+            style: TextStyle(fontStyle: FontStyle.italic),
+          )),
+        ),
+        backgroundColor: Colors.white,
+        actions: const [
+          Icon(Icons.person, color: Colors.black12),
+          Icon(
+            Icons.settings,
+            color: Colors.amber,
+          )
+        ],
+      ),
+      body: Container(
+          color: Colors.white,
+          child: SafeArea(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  width: 250.0,
+                  margin: const EdgeInsets.only(bottom: 50.0),
+                  child: const Center(
+                    child: Text("Hola mundo",
+                        style: TextStyle(color: Colors.white, fontSize: 28.0)),
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  )),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: Text(
+                    "En este curso, estamos aprendiendo Flutter, con los estudiantes de la carrera de software," +
+                        "y estudiantes apacionados con la programación y el desarrollo de aplicaciones",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black, fontSize: 18.0)),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  MaterialButton(
+                    onPressed: (){
+                      _onPressedDashboard(context);
+                    },
+                    child: const Text("Dashborad"),
+                    color: Colors.amber,
+                  ),
+                  const SizedBox(width: 10.0),
+                  MaterialButton(
+                    onPressed: () {
+                      _onPressedEstado(context);
+                    },
+                    child: const Text("Estado"),
+                    color: Colors.amber,
+                  ),
+                  const SizedBox(width: 10.0),
+                  MaterialButton(
+                    onPressed: () {
+                      _onPressedDashboard(context);
+                    },
+                    child: const Text("Cerrar sesión"),
+                    color: Colors.amber,
+                  ),
+                ],
+              )
+            ],
+          ))),
+    );
+  }
+}
